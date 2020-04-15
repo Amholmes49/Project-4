d3da11ac8706f48097f35d7cd85489ab89210764
Jared Helming <helmingjared@johndeere.com>
2020-04-15 10:49:03 -0500

    made urls

--- Iowa_Golf/Iowagolfapp/urls.py
+++ Iowa_Golf/Iowagolfapp/urls.py
@@ -1,7 +1,9 @@
 from rest_framework import routers
-from .api import PlayerViewSet
+from .api import PlayerViewSet, CourseViewSet, ScoreViewSet
 
 router = routers.DefaultRouter()
 router.register('api/players', PlayerViewSet, 'players')
+router.register('api/courses', CourseViewSet, 'courses')
+router.register('api/scores', ScoreViewSet, 'scores')
 
 urlpatterns = router.urls
