c87087b3d71ceac244bba8fe47fb982b9d04ca76
Aaron Holmes <HolmesAaron@Johndeere.com>
2020-04-20 15:05:43 -0500

    change

--- Iowa_Golf/frontend/src/components/App.js
+++ Iowa_Golf/frontend/src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Header from "./layout/Header";
 import Home from "./layout/Home";
 import Dashboard from "./Players/Dashboard";
@@ -8,10 +14,9 @@ import Dashboard_Courses from "./Courses/Dashboard_Courses";
 import Dashboard_Scores from "./Scores/Dashboard_Scores";
 import { Provider } from "react-redux";
 import store from "../store";
-import "./App.css"
+import "./App.css";
 import Courses from "./Courses/Courses";
 import Form from "./Scores/Form";
-
 
 class App extends Component {
   render() {
@@ -20,15 +25,16 @@ class App extends Component {
         <Router>
           <Header />
 
-          <div className="container">
-
-          </div>
+          <div className="container"></div>
 
           <Switch>
             <Route path="/" exact component={Courses} />
             <Route path="/createplayer" component={Dashboard} />
             <Route path="/createcourse" component={Dashboard_Courses} />
-            <Route path="/courses/:id" render={(props) => <Form {...props} />} />
+            <Route
+              path="/courses/:id"
+              render={(props) => <Form {...props} />}
+            />
           </Switch>
         </Router>
       </Provider>
