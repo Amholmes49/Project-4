4c1d119a9540288816ee1afc0f9a4c2e71a0877e
Aaron Holmes <HolmesAaron@Johndeere.com>
2020-04-21 11:18:17 -0500

    users stories added to readme post change

--- Iowa_Golf/frontend/src/actions/courses.js
+++ Iowa_Golf/frontend/src/actions/courses.js
@@ -27,7 +27,7 @@ export const deleteCourse = (id) => (dispatch) => {
 
 export const addCourse = (course) => (dispatch) => {
   axios
-    .post("https://iowa-golf-newapp.herokuapp.com/api/courses/", course)
+    .post("/api/courses/", course)
     .then((res) => {
       dispatch({
         type: ADD_COURSE,
